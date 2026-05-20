6d5ffcf7aabe9c74b6973f6bd024446a0dca1b5d
Ruslan Pryimak <ruslan.pryimak@drivemode.com>
2026-05-20 13:20:10 -0700

    Initial Commit

---
+++ NotToday Watch App/NotTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotTodayApp.swift
+//  NotToday Watch App
+//
+//  Created by Ruslan Pryimak on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct NotToday_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
